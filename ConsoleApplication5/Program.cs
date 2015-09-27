4f0ddb26346d2b7699e57b00311b8659ee87e3d8
Your Name <you@example.com>
2015-09-27 19:05:59 -0400

    5

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("What is your name?");
+               string x = Console.ReadLine();
+                if (x[0] == 'A')
+                {
+                    Console.WriteLine("Go to the front.");
+                
+
+            }
+
+        }
+      
+        }
+    }
+}
